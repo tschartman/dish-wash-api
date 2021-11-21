57846d1a35d27a6242eb12583ff2b3c7c17a9926
tim <tschartman@yahoo.com>
2021-11-20 20:40:52 -0600

    base app

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+require('dotenv').config()
+const bodyParser= require('body-parser') 
+const app = express()  
+const cors = require('cors')
+app.use(express.json())
+const { MongoClient } = require('mongodb');
+
+app.use(cors())
+
+const uri = `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    
+    app.listen(3000, function() {
+        console.log('listening on 3000')
+    })
+    
+    const db = client.db("dishwasherDB")
+
+    app.get('/washer/status', function (request, response) {
+        const status = db.collection('dishwashers').find().toArray((err, res) => {
+            response.send(res)
+        })
+    })
+    
+    app.post('/washer/update', async (req, res) => {
+        const clean = req.body.clean;
+        const updatedResult = await db.collection('dishwashers').updateOne({}, { $set: {clean: clean}})
+        console.log(updatedResult)
+        res.send(updatedResult)
+    })
+});
+
+
+
