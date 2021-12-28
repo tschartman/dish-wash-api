f77cd7fd4006a3619255c36124043d15fe2e6cf9
tim <tschartman@yahoo.com>
2021-12-28 12:35:30 -0600

    grovery list

---
+++ src/api/controllers/grocery.js
@@ -0,0 +1,36 @@
+const MongoDB  = require('../utils/mongo');
+const db = MongoDB.getDB()
+const discord = require('../utils/discord');
+const dclient = discord.getDiscord();
+
+const getItems = async (request, response, next) => {
+    const status = db.db("dishwasherDB").collection('grocery').find().toArray((err, res) => {
+        if (err) {
+            console.log(err)
+            response.send("Error")
+        }
+        response.send(res)
+    })
+}
+
+const insertItem = async (request, response, next) => {
+    const item = request.body.name;
+    const updatedResult = await db.db("dishwasherDB").collection('grocery').insertOne({name: item})
+    const channel = dclient.channels.cache.find(channel => channel.name === "general")
+    //channel.send(`${item} has been added to the grocery list`)
+    response.send(updatedResult)
+}
+
+const deleteItem = async (request, response, next) => {
+    const item = request.body.name;
+    const updatedResult = await db.db("dishwasherDB").collection('grocery').deleteOne({name: item})
+    const channel = dclient.channels.cache.find(channel => channel.name === "general")
+    //channel.send(`${item} has been removed from the grocery list`)
+    response.send(updatedResult)
+}
+
+module.exports = {
+    getItems,
+    insertItem,
+    deleteItem
+}
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router(); 
 const dishwasherController = require('../controllers/dishwasher'); 
 const trashController = require('../controllers/trash')
+const groceryController = require('../controllers/grocery')
 
 // Dishwasher
 router.get('/washer/status', dishwasherController.getWasherStatus)
@@ -13,5 +14,10 @@ router.get('/trash/collector', trashController.getCollector)
 router.get('/trash/count', trashController.getCount)
 router.post('/trash/collector', trashController.updateCollector)
 router.get('/trash/notify', trashController.notifyCollector)
+
+// Grocery
+router.get('/grocery', groceryController.getItems)
+router.post('/grocery', groceryController.insertItem)
+router.delete('/grocery', groceryController.deleteItem)
 
 module.exports = router;
