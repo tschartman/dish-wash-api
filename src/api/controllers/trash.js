10aa55c9542a9d7c4f7f25be2070b08e7e08afb7
tim <tschartman@yahoo.com>
2021-12-28 11:16:39 -0600

    notify next collector

--- src/api/controllers/trash.js
+++ src/api/controllers/trash.js
@@ -33,21 +33,13 @@ const updateCollector = async (request, response, next) => {
         const {collectors} = await db.db("dishwasherDB").collection('trash').findOne({name: "collectors"})
         const {collector} = await db.db("dishwasherDB").collection('trash').findOne({name: "collector"})
         const updatedResult1 = await db.db("dishwasherDB").collection('trash').updateOne({"count.key": collector}, { $inc: { 'count.$.count' : 1 }})
+        const nextCollector = (collector + 1) % 4
         const channel = dclient.channels.cache.find(channel => channel.name === "general")
-        channel.send(`${collectors[collector]} has taken out the trash`)
+        channel.send(`${collectors[collector]} has taken out the trash ${collectors[nextCollector]} it is your turn to take the trash`)
     }
     const updatedResult = await db.db("dishwasherDB").collection('trash').updateOne({name: "collector"}, { $set: {collector: newCollector}})
     response.send(updatedResult)
 }
-
-// const updateWasherStatus = async (request, response, next) => {
-//     const clean = request.body.clean;
-//     const updatedResult = await db.db("dishwasherDB").collection('dishwashers').updateOne({}, { $set: {clean: clean}})
-//     const channel = dclient.channels.cache.find(channel => channel.name === "general")
-//     const status = clean ? 'Clean' : 'Dirty'
-//     channel.send(`Dishwasher is ${status}`)
-//     response.send(updatedResult)
-// }
 
 module.exports = {
     getCollectors,
